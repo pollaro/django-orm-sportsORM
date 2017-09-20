5d6418ad0572a646efd56fbb6881ad60349012da
Jim <jim.pollaro@gmail.com>
2017-09-20 08:22:40 -0700

    All views done

--- apps/leagues/views.py
+++ apps/leagues/views.py
@@ -5,9 +5,42 @@ from . import team_maker
 
 def index(request):
 	context = {
-		"leagues": League.objects.all(),
-		"teams": Team.objects.all(),
-		"players": Player.objects.all(),
+		#Baseball only:
+		"leagues":League.objects.filter(sport='Baseball')
+		#Womens only leagues:
+		'leagues':League.objects.filter(name__contains='Womens')
+		#Hockey leagues:
+		'leagues':League.objects.filter(name__contains='Hockey')|League.objects.filter(sport__contains='Hockey')
+		#Non football sports:
+		'leagues':League.objects.exclude(sport='Football')
+		#Leagues that are "conferences":
+		'leagues':League.objects.filter(name__contains='Conference')
+		#Leagues in the atlantic region
+		'leagues':League.objects.filter(name__contains='Atlantic')
+		#Teams Based in Dallas
+		'teams':Team.objects.filter(location__contains='Dallas')
+		#Teams Named Raptors
+		'teams':Team.objects.filter(team_name__contains='Raptors')
+		#Teams in Cities that have City in them
+		'teams':Team.objects.filter(location__contains='City')
+		#Teams that begin with T
+		'teams':Team.objects.filter(team_name__startswith='T')
+		#All teams ordered by location
+		'teams':Team.objects.order_by('location')
+		#All teams ordered by reverse team name
+		'teams':Team.objects.order_by('-team_name')
+		#Players with last name Cooper
+		"players": Player.objects.filter(last_name='Cooper')
+		#Players with first name Joshua
+		"players": Player.objects.filter(first_name='Joshua')
+		#Players with last name cooper except ones with Joshua
+		"players": Player.objects.filter(last_name='Cooper').exclude(first_name='Joshua')
+		#Players with first name Alexander or Wyatt
+		"players": Player.objects.filter(first_name='Alexander')|Player.objects.filter(first_name='Wyatt')
+
+		# "leagues": League.objects.all(),
+		# "teams": Team.objects.all(),
+		# "players": Player.objects.all(),
 	}
 	return render(request, "leagues/index.html", context)
 
